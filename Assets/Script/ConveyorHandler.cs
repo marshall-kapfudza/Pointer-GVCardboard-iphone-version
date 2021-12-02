88cd615a9c8448a9ac58513804a84022105eea6c
BMCC-Turcios <92555359+BMCC-Turcios@users.noreply.github.com>
2021-12-02 14:46:49 -0500

    Fixed bug when nodes stop the conveyor is still on

--- Assets/Script/ConveyorHandler.cs
+++ Assets/Script/ConveyorHandler.cs
@@ -8,7 +8,7 @@ public class ConveyorHandler : MonoBehaviour
 
     [field: SerializeField]
     public List<NodeDetection> Belts { get; private set; }
-
+    private bool isBeltOn;
     private void Awake()
     {
         Belts = new List<NodeDetection>();
@@ -16,6 +16,7 @@ public class ConveyorHandler : MonoBehaviour
 
     private void Start()
     {
+        isBeltOn = false;
         NodeDetection belt = null;
         foreach(Transform child in transform)
         {
@@ -24,22 +25,41 @@ public class ConveyorHandler : MonoBehaviour
                 Belts.Add(belt);
         }
 
-        
+        ChangeConveyorBeltState(ConveyorDirection.LEFT);
 
-        
+    }
+    private void Update()
+    {
+        if (!isBeltOn) return;
+        if(NodeDetection.ActiveNodes == 2)
+        {
+            ChangeConveyorBeltState(ConveyorDirection.STOP);
+        }
     }
     //Turns all the convoyer on and move nodes to right left or stop
     public void ChangeConveyorBeltState(float direction)
     {
+        isBeltOn = true;
+        NodeDetection.ResetActiveNodes();
         foreach(var belt in Belts)
         {
             belt.ActivateConveyor(direction);
         }
     }
-    [ContextMenu("Turn On Conveyor")]
-    public void turnon()
+    [ContextMenu("Turn On Conveyor to left")]
+    public void Demoturnleft()
     {
         ChangeConveyorBeltState(ConveyorDirection.LEFT);
+    }
+    [ContextMenu("Turn On Conveyor to right")]
+    public void Demoturnright()
+    {
+        ChangeConveyorBeltState(ConveyorDirection.RIGHT);
+    }
+    [ContextMenu("Stop Conveyor")]
+    public void DemoStop()
+    {
+        ChangeConveyorBeltState(ConveyorDirection.STOP);
     }
     //shift nodes right and insert the node at the currentNode;
     public void InsertNode()
--- Assets/Script/NodeDetection.cs
+++ Assets/Script/NodeDetection.cs
@@ -13,13 +13,15 @@ public class NodeDetection : MonoBehaviour
     public  GameObject Node { get; private set; }
     [field: SerializeField]
     public PCSConfig Conveyor { get; private set; }
-
+    public static int ActiveNodes { get; private set; }
     private void Start()
     {
+        ActiveNodes = 0;
         Conveyor = transform.parent.GetComponentInChildren<PCSConfig>();
     }
     private void OnTriggerEnter(Collider other)
     {
+        ActiveNodes++;
         Node = other.gameObject;
         ActivateConveyor(ConveyorDirection.STOP);
     }
@@ -28,7 +30,10 @@ public class NodeDetection : MonoBehaviour
     {
         Node = null;
     }
-
+    public static void ResetActiveNodes()
+    {
+        ActiveNodes = 0;
+    }
     public void ActivateConveyor(float direction)
     {
         Conveyor.SetSpeed(direction);
