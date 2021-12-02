d0b0a72da1ccbbf6fbb0d7c7364e9907c5d59220
BMCC-Turcios <92555359+BMCC-Turcios@users.noreply.github.com>
2021-12-01 23:16:48 -0500

    Changed List in ConveyorHanler to access the NodeDetection

--- Assets/Script/ConveyorHandler.cs
+++ Assets/Script/ConveyorHandler.cs
@@ -1,38 +1,36 @@
 using System.Collections.Generic;
 using UnityEngine;
-using PCS;
 
-public static class ConveyorSpeed
-{
-    const float STOP = 0f;
-    const float LEFT = 1;
-    const float RIGHT = -1;
-}
+
 public class ConveyorHandler : MonoBehaviour
 {
-    private List<PCSConfig> ConveyorBelt;
+    public List<NodeDetection> ConveyorBelt { get; private set; }
 
     private void Awake()
     {
-        ConveyorBelt = new List<PCSConfig>();
+        ConveyorBelt = new List<NodeDetection>();
     }
 
     private void Start()
     {
-        PCSConfig belt = null;
+        NodeDetection belt = null;
         foreach(Transform child in transform)
         {
-            belt = child.GetComponentInChildren<PCSConfig>();
+            belt = child.GetComponentInChildren<NodeDetection>();
             if(belt != null)
                 ConveyorBelt.Add(belt);
         }
+
+        ChangeConveyorBeltState(ConveyorDirection.LEFT);
+
+        
     }
     //Turns all the convoyer on and move nodes to right left or stop
     public void ChangeConveyorBeltState(float direction)
     {
         foreach(var belt in ConveyorBelt)
         {
-            belt.SetSpeed(direction);
+            belt.ActivateConveyor(direction);
         }
     }
 
--- Assets/Script/NodeDetection.cs
+++ Assets/Script/NodeDetection.cs
@@ -1,20 +1,33 @@
 using UnityEngine;
 using PCS;
+
+public static class ConveyorDirection
+{
+    public const float STOP = 0f;
+    public const float LEFT = 1;
+    public const float RIGHT = -1;
+}
 public class NodeDetection : MonoBehaviour
 {
     
     public  GameObject Node { get; private set; }
-    private PCSConfig Conveyor;
+    public PCSConfig Conveyor { get; private set; }
 
     private void Start()
     {
-        Conveyor = transform.parent.gameObject.GetComponentInChildren<PCSConfig>();
+        Conveyor = transform.parent.GetComponentInChildren<PCSConfig>();
     }
     private void OnTriggerEnter(Collider other)
     {
         Node = other.gameObject;
-        Conveyor.SetSpeed(0);
+        ActivateConveyor(ConveyorDirection.STOP);
     }
+
+    public void ActivateConveyor(float direction)
+    {
+        Conveyor.SetSpeed(direction);
+    }
+
     private void OnTriggerExit()
     {
         Node = null;
