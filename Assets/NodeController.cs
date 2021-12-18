1e6f527673cbccc2b508b50c89d5f953dda9ac9d
BMCC-Turcios <92555359+BMCC-Turcios@users.noreply.github.com>
2021-12-17 22:04:08 -0500

    Added EventTrigger for Node

---
+++ Assets/NodeController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeController : MonoBehaviour
+{
+    public void OnUserClick()
+    {
+
+    }
+    public void OnUserHover()
+    {
+
+    }
+    public void OnUserExit()
+    {
+
+    }
+}
